2357c601b448b39ffad226d8fa42f0cd5783941f
Abiola David <davidolanrewaju2000@gmail.com>
2022-06-29 15:14:56 +0100

    Initialized project

---
+++ src/main/java/com/sirdave/lendingplatform/LendingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sirdave.lendingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingPlatformApplication.class, args);
+	}
+
+}
